c4938a9e03b8c83d71d9b066d4eca6c269af617d
Alexander Brock <alexander@lunar-orbit.de>
2018-03-07 19:48:16 +0100

    fix bug

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+class Interval {
+public:
+    enum class size {
+        unison = 0,
+        minor_second = 1,
+        major_second = 2,
+        minor_third = 3,
+        major_third = 4,
+        perfect_fourth = 5,
+        augmented_fourth = 6,
+        perfect_fifth = 7,
+        minor_sixth = 8,
+        major_sixth = 9,
+        minor_seventh = 10,
+        major_seventh = 11,
+        octave = 12,
+        minor_ninth = 13,
+        major_ninth = 14,
+        minor_tenth = 15,
+        major_tenth = 16
+    };
+};
+
+class Create {
+private:
+
+
+public:
+
+    void run() {}
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Create c;
+    c.run();
     return 0;
 }
